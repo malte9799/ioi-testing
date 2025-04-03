d6db733f167b47a12046c45f4c457724df12094a
[MΛLTE] <maltem.hilpert@gmx.de>
2025-04-03 12:10:34 +0200

    misc: cleanup

--- !files.js
+++ !files.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const fs = require('fs');
+import { join } from 'path';
+import { readdirSync, writeFileSync } from 'fs';
 const src = __dirname;
 const ignore = ['.vscode', 'ioi-testing.zip', '.gitignore', '.git'];
 
 const files = [];
 const dirs = [];
 let f = (_) => {
-	fs.readdirSync(_, { withFileTypes: true }).forEach((v) => {
+	readdirSync(_, { withFileTypes: true }).forEach((v) => {
 		if (ignore.includes(v.name)) return;
 		if (v.name.startsWith('!')) return;
-		let p = path.join(_, v.name);
-		let s = path.join(src, p);
+		let p = join(_, v.name);
+		let s = join(src, p);
 		if (v.isDirectory()) {
 			dirs.push(p);
 			f(p);
@@ -19,10 +19,10 @@ let f = (_) => {
 };
 f('.');
 
-fs.writeFileSync('./!files.txt', files.join('\n'));
-fs.writeFileSync('./!dirs.txt', dirs.join('\n'));
+writeFileSync('./!files.txt', files.join('\n'));
+writeFileSync('./!dirs.txt', dirs.join('\n'));
 
-module.exports = {
+export default {
 	files,
 	dirs,
 };
